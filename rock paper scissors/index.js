449264aec7286d1f2f317dced37fb85203ad1962
Kyle Smith <ksmithj94@gmail.com>
2022-11-25 21:30:35 -0500

    updates to RPS game

--- rock paper scissors/index.js
+++ rock paper scissors/index.js
@@ -1,28 +1,48 @@
 const message = document.querySelector(".message");
+const heading = document.querySelector("h2");
 const buttons = document.querySelectorAll("button");
 
 for(let i = 0; i <buttons.length; i++){
 
   buttons[i].addEventListener("click", function(e) {
 
-    let playerGuess = e.target.innerText  
+    let playerGuess = e.target.innerText;  
     let selection = Math.floor(Math.random()*3); 
     let gameArray = ["Rock","Paper","Scissors"];
     let computerGuess = gameArray[selection];
+    let score = [0,0]
 
       console.log(computerGuess);
       console.log(playerGuess);
 
     if(playerGuess == "Rock" && computerGuess == "Paper") {
-    console.log("C Wins")
+      heading.innerHTML = "Computer Wins!"
+      message.innerHTML = "Player chose: " + playerGuess + "<br> Computer chose: " + computerGuess 
     }
-    else if (playerGuess == "Paper" && computerGuess == "Scissors") {
-      console.log("C Wins")
+    else if (playerGuess == "Rock" && computerGuess == "Scissors") {
+      heading.innerHTML = "Player Wins!"
+      message.innerHTML = "Player chose: " + playerGuess + "<br> Computer chose: " + computerGuess 
+    }
+    else if (playerGuess == "Paper" &&    computerGuess == "Scissors") {
+      heading.innerHTML = "Computer Wins!"
+      message.innerHTML = "Player chose: " + playerGuess + "<br> Computer chose: " + computerGuess 
+    }
+    else if (playerGuess == "Paper" && computerGuess == "Rock") {
+      heading.innerHTML = "Player Wins!"
+      message.innerHTML = "Player chose: " + playerGuess + "<br> Computer chose: " + computerGuess 
     }
     else if (playerGuess == "Scissors" && computerGuess == "Rock"){
-      console.log("C Wins")
+      heading.innerHTML = "Computer Wins!"
+      message.innerHTML = "Player chose: " + playerGuess + "<br> Computer chose: " + computerGuess 
     }
-
+    else if (playerGuess == "Scissors" && computerGuess == "Paper") {
+      heading.innerHTML = "Player Wins!"
+      message.innerHTML = "Player chose: " + playerGuess + "<br> Computer chose: " + computerGuess 
+    }
+    else {
+      heading.innerHTML = "It's A Tie!"
+      message.innerHTML = ""
+    }
   });
-}
+};
 
