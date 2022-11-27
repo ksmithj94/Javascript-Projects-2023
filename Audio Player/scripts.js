d0d6951e12916ce3c104babe706c3ed22830097b
Kyle Smith <ksmithj94@gmail.com>
2022-11-26 19:06:25 -0500

    starting animal audio project files

---
+++ Audio Player/scripts.js
@@ -0,0 +1,8 @@
+const lion = document.querySelector(".lionClass");
+const tiger = document.querySelector(".tigerClass");
+const dog = document.querySelector(".dogClass");
+
+lion.addEventListener("click", function(){
+    console.log("click");
+  }
+);
