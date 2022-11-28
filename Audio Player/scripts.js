4e326b21a473fb17fd49589a8147f015792c168a
Kyle Smith <ksmithj94@gmail.com>
2022-11-27 20:04:25 -0500

    updates to Anaiml Audio

--- Audio Player/scripts.js
+++ Audio Player/scripts.js
@@ -1,8 +1,20 @@
 const lion = document.querySelector(".lionClass");
 const tiger = document.querySelector(".tigerClass");
 const dog = document.querySelector(".dogClass");
+const lionSound = new Audio('/Audio Player/sounds/roaring_lion_sns.wav');
 
 lion.addEventListener("click", function(){
-    console.log("click");
+    console.log("lick lion");
+    lionSound.play();
+  }
+);
+
+tiger.addEventListener("click", function(){
+  console.log("click tiger");
+  } 
+);
+
+dog.addEventListener("click", function(){
+  console.log("click dog");
   }
 );
